67083e71fa9f2d18a877d0f09e943d1e6bf4cb08
LiqunHu <huliquns@126.com>
2024-01-09 10:48:21 +0800

    update test

--- test/Tornado.js
+++ test/Tornado.js
@@ -285,17 +285,19 @@ describe('ETHTornado', async function () {
     await snarkjs.wtns.calculate(unstringifyBigInts(input), circuit_wasm, wtns)
 
     //groth16 proof
-    let res = await snarkjs.groth16.prove(path.join(
-      __dirname,
-      'circuits',
-      'withdraw_proving.zkey'
-    ), wtns)
+    let res = await snarkjs.groth16.prove(
+      path.join(__dirname, 'circuits', 'withdraw_proving.zkey'),
+      wtns,
+    )
 
     const proofA = [res.proof.pi_a[0], res.proof.pi_a[1]]
-    const proofB = [[res.proof.pi_b[0][1], res.proof.pi_b[0][0]], [res.proof.pi_b[1][1], res.proof.pi_b[1][0]]]
+    const proofB = [
+      [res.proof.pi_b[0][1], res.proof.pi_b[0][0]],
+      [res.proof.pi_b[1][1], res.proof.pi_b[1][0]],
+    ]
     const proofC = [res.proof.pi_c[0], res.proof.pi_c[1]]
 
-    return (proofA, proofB, proofC, res.publicSignals)
+    return { proofA, proofB, proofC, publicSignals: res.publicSignals }
   }
 
   before(async () => {
@@ -387,12 +389,16 @@ describe('ETHTornado', async function () {
           pathElements: pathElements,
           pathIndices: pathIndices,
         }
-        const { proofA, proofB, proofC ,publicSignals } = await generateSnarkProofFromFile(input)
+        const { proofA, proofB, proofC, publicSignals } =
+          await generateSnarkProofFromFile(input)
         // const proof = '0x2a5e6cfc496a8532b0076a5675da967923e796e9209ffbec971d80288445677c26e369d0ff2de2f36839a024287cfa70f9c0e956b1cbaa37fdf6b0693d7bb646289e82c3a3ad1c919bc3c950aeadc7d17ee0baa1c3ee694d0ae1ce1062d87e660423acd3a763bcd40a9797beaba5542a361ceee2e4662028221ff942e5ad3bf927dcbdd4477bee6a1a87aba4d2b3b3c4cfdb17658404f0b5646fe641ef9c5368304046d6a94b3acf42ea54f16c2bab3dbe546f17f7884aeef59b3b2364a327e20bb5297ae039ad1b4c75f50f22086fa71ea0242d393d286853d7e1d6370477711debcf7f7b47748f44456e045b7a5e6ecad68fc240cb827d4f5c54130d024e83'
         balance = await ethers.provider.getBalance(operator)
         console.log(ethers.formatEther(balance))
         tx = await tornadoInstance.withdraw(
-          proofA, proofB, proofC ,publicSignals,
+          proofA,
+          proofB,
+          proofC,
+          publicSignals,
           toFixedHex(input.root),
           toFixedHex(input.nullifierHash),
           toFixedHex(input.recipient, 20),
